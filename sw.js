3b21db16cb0c36da54f8e8b75a0ac61b3999d141
volts1922 <volts1922@yahoo.com>
2026-06-25 16:45:22 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dc-payroll-v30';
+const CACHE_NAME = 'dc-payroll-v30.6';
 const ASSETS_TO_CACHE = ['/dental_city_payroll/index.html','/dental_city_payroll/manifest.json'];
 
 self.addEventListener('install', (event) => {
