fdbcb035be506a6b7f659f49f92024d9de57024c
volts1922 <volts1922@yahoo.com>
2026-06-28 08:13:43 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-// DC Payroll Service Worker v30.58
-const VERSION = 'v30.58-tracker-complete-fix';
-const CACHE = 'dcpayroll-v30.58';
+// DC Payroll Service Worker v30.59
+const VERSION = 'v30.59-tracker-monthly-annual-fix';
+const CACHE = 'dcpayroll-v30.59';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => !k.includes('v30.58')).map(k => caches.delete(k)))));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => !k.includes('v30.59')).map(k => caches.delete(k)))));
   self.clients.claim();
 });
 
@@ -21,4 +21,4 @@ self.addEventListener('fetch', e => {
   );
 });
 
-console.log('[SW] v30.58 ready');
+console.log('[SW] v30.59 ready');
