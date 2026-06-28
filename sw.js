22682cbf27ce2cdbdc23fadc8493cee7fbaa16a7
volts1922 <volts1922@yahoo.com>
2026-06-28 13:39:25 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-// DC Payroll Service Worker v30.74
-const VERSION = 'v30.74-simplified-payslip-ui';
-const CACHE = 'dcpayroll-v30.74';
+// DC Payroll Service Worker v30.75
+const VERSION = 'v30.75-fix-table-names';
+const CACHE = 'dcpayroll-v30.75';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => !k.includes('v30.74')).map(k => caches.delete(k)))));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => !k.includes('v30.75')).map(k => caches.delete(k)))));
   self.clients.claim();
 });
 
@@ -21,4 +21,4 @@ self.addEventListener('fetch', e => {
   );
 });
 
-console.log('[SW] v30.74 ready');
+console.log('[SW] v30.75 ready');
